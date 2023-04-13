ffcd7de4e5120ec969969d2432baa56e072a1eb3
JoaoG <joaogabriel@protonmail.com>
2023-04-13 18:05:00 -0300

    adding tests

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,67 +4,81 @@ import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
 import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
-export async function transactionsRoutes (app: FastifyInstance): Promise<void> {
-  app.addHook('preHandler', async (request, reply) => {
-    console.log(`${request.method} ${request.url}`)
-  })
+export async function transactionsRoutes(app: FastifyInstance): Promise<void> {
+  // app.addHook('preHandler', async (request, reply) => {
+  //   console.log(`${request.method} ${request.url}`)
+  // })
 
-  app.get('/', { preHandler: [checkSessionIdExists] }, async (request, reply) => {
-    const { sessionId } = request.cookies
-    const transactions = await knex('transactions')
-      .where('session_id', sessionId)
-      .select()
+  app.get(
+    '/',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const { sessionId } = request.cookies
+      const transactions = await knex('transactions')
+        .where('session_id', sessionId)
+        .select()
 
-    return { transactions }
-  })
+      return { transactions }
+    },
+  )
 
   app.get('/:id', { preHandler: [checkSessionIdExists] }, async (request) => {
     const getTransactionParamsSchema = z.object({
-      id: z.string().uuid()
+      id: z.string().uuid(),
     })
     const { sessionId } = request.cookies
 
     const { id } = getTransactionParamsSchema.parse(request.params)
 
-    const transaction = await knex('transactions').where({ session_id: sessionId, id }).first()
+    const transaction = await knex('transactions')
+      .where({ session_id: sessionId, id })
+      .first()
 
     return { transaction }
   })
 
-  app.get('/summary', { preHandler: [checkSessionIdExists] }, async (request) => {
-    const { sessionId } = request.cookies
-    const summary = await knex('transactions').where('session_id', sessionId).sum('amount', { as: 'amount' }).first()
+  app.get(
+    '/summary',
+    { preHandler: [checkSessionIdExists] },
+    async (request) => {
+      const { sessionId } = request.cookies
+      const summary = await knex('transactions')
+        .where('session_id', sessionId)
+        .sum('amount', { as: 'amount' })
+        .first()
 
-    return { summary }
-  })
+      return { summary }
+    },
+  )
 
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
-      type: z.enum(['credit', 'debit'])
+      type: z.enum(['credit', 'debit']),
     })
 
-    const { title, type, amount } = createTransactionBodySchema.parse(request.body)
+    const { title, type, amount } = createTransactionBodySchema.parse(
+      request.body,
+    )
 
     let sessionId = request.cookies.sessionId
 
     if (sessionId == null) {
       sessionId = randomUUID()
 
-      void reply.cookie('sessionId', sessionId, {
+      reply.cookie('sessionId', sessionId, {
         path: '/',
-        maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
       })
     }
 
-    await knex('transactions')
-      .insert({
-        id: randomUUID(),
-        title,
-        amount: type === 'credit' ? amount : amount * (-1),
-        session_id: sessionId
-      })
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
+    })
 
     return await reply.status(201).send()
   })
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,11 @@
-import fastify from 'fastify'
+import { app } from './app'
 import { env } from './env'
-import cookie from '@fastify/cookie'
-import { transactionsRoutes } from './routes/transactions'
 
-const app = fastify()
-
-void app.register(cookie)
-
-void app.register(transactionsRoutes, {
-  prefix: 'transactions'
-})
-
-app.listen({ port: env.PORT }).then(() => {
-  console.log('HTTP server is running')
-}).catch(err => {
-  console.error(err)
-})
+app
+  .listen({ port: env.PORT })
+  .then(() => {
+    console.log('HTTP server is running')
+  })
+  .catch((err) => {
+    console.error(err)
+  })
